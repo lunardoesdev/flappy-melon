8ae2aefb436cad0bc883c55650ae0abce1ede33e
Lunar <lunarlifeburst@gmail.com>
2026-08-14 00:44:27 +1000

    readme; solves #16

---
+++ README.md
@@ -0,0 +1,10 @@
+# Flappy Melon
+
+This is my first project being developed in different way
+from what i did in the past. This time I plan and create tasks
+(i put them in issues), and I do those tasks slowly and one
+by one, one at a time, without thinking of any other tasks
+than one i'm doing.
+
+The game is a flappy bird clone, nothing to be too proud of,
+but it's still should be playable and fun.
---
+++ pipe_pair.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+@export var gap_percent: float = 25.0
+@export var fallback_pipe_height: float = 128.0
+
+@onready var upper_pipe: Sprite2D = $upperPipe
+@onready var lower_pipe: Sprite2D = $lowerPipe
+
+func _ready() -> void:
+	get_viewport().size_changed.connect(_layout)
+	_layout()
+
+func _layout() -> void:
+	var screen_h := get_viewport_rect().size.y
+	var gap := screen_h * gap_percent / 100.0
+	var pipe_h := _pipe_height()
+	
+	upper_pipe.position.y =  gap / 2.0 + pipe_h / 2.0
+	lower_pipe.position.y = -gap / 2.0 - pipe_h / 2.0
+
+func _pipe_height() -> float:
+	if upper_pipe.texture != null:
+		return float(upper_pipe.texture.get_height())
+	return fallback_pipe_height
