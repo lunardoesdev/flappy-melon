d5639e5b48c57da1d342985ee27096bb70b53436
Lunar <lunarlifeburst@gmail.com>
2026-08-14 13:22:22 +1000

    reactive layout change

--- pipe_pair.gd
+++ pipe_pair.gd
@@ -1,10 +1,17 @@
 @tool
 extends Node2D
 
-@export var gap_percent: float = 25.0
-@export var gap_offset: float = 0
-@export var fallback_pipe_height: float = 128.0
+@export var gap_percent: float = 25.0:
+	set(val):
+		gap_percent = val
+		_layout()
+@export var gap_offset: float = 0:
+	set(val):
+		gap_offset = val
+		_layout()
 
+
+@onready var fallback_pipe_height: float = get_viewport_rect().size.y / 2
 @onready var upper_pipe: Sprite2D = $upperPipe
 @onready var lower_pipe: Sprite2D = $lowerPipe
 
@@ -13,11 +20,13 @@ func _ready() -> void:
 	_layout()
 
 func _layout() -> void:
+	if not is_inside_tree():
+		return
 	var screen_h := get_viewport_rect().size.y
 	var gap := screen_h * gap_percent / 100.0
 	var gap_center := screen_h * gap_offset / 100 
 	var pipe_h := _pipe_height()
-	
+		
 	upper_pipe.position.y = -gap / 2.0 - pipe_h / 2.0 + gap_center
 	lower_pipe.position.y =  gap / 2.0 + pipe_h / 2.0 + gap_center
 
